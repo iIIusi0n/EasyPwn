d9cccb57a92e2524c9caa532651d3241feb5b0f7
iiiusi0n <iiiusi0n@wyvern.pw>
2025-01-04 23:50:13 +0900

    make chatbot service works!

--- internal/service/chatbot.go
+++ internal/service/chatbot.go
@@ -3,10 +3,13 @@ package service
 import (
 	"context"
 	pb "easypwn/internal/api"
+	"easypwn/internal/pkg/prompt"
+
+	openai "github.com/sashabaranov/go-openai"
 )
 
 type ChatbotService struct {
-	openAiApiKey   string
+	openaiClient   *openai.Client
 	instanceClient pb.InstanceClient
 
 	pb.UnimplementedChatbotServer
@@ -14,7 +17,57 @@ type ChatbotService struct {
 
 func NewChatbotService(ctx context.Context, openAiApiKey string, instanceClient pb.InstanceClient) *ChatbotService {
 	return &ChatbotService{
-		openAiApiKey:   openAiApiKey,
+		openaiClient:   openai.NewClient(openAiApiKey),
 		instanceClient: instanceClient,
 	}
+}
+func (s *ChatbotService) getLogs(ctx context.Context, instanceId string) (string, error) {
+	logs, err := s.instanceClient.GetInstanceLogs(ctx, &pb.GetInstanceLogsRequest{
+		InstanceId: instanceId,
+		Limit:      30,
+	})
+	if err != nil {
+		return "", err
+	}
+	return logs.Logs, nil
+}
+
+func (s *ChatbotService) ExecuteCompletion(ctx context.Context, logs string, message string) (string, error) {
+	resp, err := s.openaiClient.CreateChatCompletion(ctx, openai.ChatCompletionRequest{
+		Model: openai.GPT4o,
+		Messages: []openai.ChatCompletionMessage{
+			{
+				Role:    openai.ChatMessageRoleSystem,
+				Content: prompt.SystemPrompt,
+			},
+			{
+				Role:    openai.ChatMessageRoleUser,
+				Content: logs,
+			},
+			{
+				Role:    openai.ChatMessageRoleUser,
+				Content: prompt.LogGuidePrompt,
+			},
+			{
+				Role:    openai.ChatMessageRoleUser,
+				Content: message,
+			},
+		},
+	})
+	if err != nil {
+		return "", err
+	}
+	return resp.Choices[0].Message.Content, nil
+}
+
+func (s *ChatbotService) GetResponse(ctx context.Context, req *pb.GetResponseRequest) (*pb.GetResponseResponse, error) {
+	logs, err := s.getLogs(ctx, req.InstanceId)
+	if err != nil {
+		return nil, err
+	}
+	response, err := s.ExecuteCompletion(ctx, logs, req.Message)
+	if err != nil {
+		return nil, err
+	}
+	return &pb.GetResponseResponse{Response: response}, nil
 }
